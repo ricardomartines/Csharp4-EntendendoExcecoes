a439517509c20aa16eff5fe743135ab6f9a20f94
RICARDO MARTINES <ricardo.martines@totvs.com.br>
2019-01-24 17:36:40 -0200

    24/01/2019

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -8,13 +8,63 @@ namespace ByteBank
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            /*
             ContaCorrente conta = new ContaCorrente(7480, 874150);
             Console.WriteLine(ContaCorrente.TaxaOperacao);
         
 
             Console.ReadLine();
+            */
+            try
+            {
+                Metodo();
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Não é possivel duvidir por zero !");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+
+        public static int Dividir(int numero, int divisor)
+        {
+            //ContaCorrente conta = null;
+            //Console.WriteLine(conta.Saldo);
+
+            try
+            {
+                return numero / divisor;
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Exceção com o numero: " + numero + " dividido por: " + divisor);
+                throw;
+            }
+        }
+
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+
+        }
+
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
         }
     }
 }
